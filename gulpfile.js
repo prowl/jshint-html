08e4d61f7d7d5c446c37adb063b336b3352eb6f5
Joshwa Fugett <jfugett27@gmail.com>
2014-08-29 14:15:13 -0700

    feat(init): Initial release

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var paths = {
+  root: __dirname,
+  soften: ['**/*.js', '**/*.json', '!./node_modules/**'],
+  src: ['**/*.js', '!./node_modules/**'],
+  specs: ['specs/**/*.js'],
+  statements_threshold: 90,
+  functions_threshold: 100,
+  branches_threshold: 100,
+  lines_threshold: 80
+};
+
+require('./lib/gulp')(gulp, paths);
