6cdfc83bf17ffcb66acd0297288b6a78a409b8c6
Joshwa Fugett <jfugett27@gmail.com>
2014-09-01 19:28:28 -0700

    fix(docs): Adding docs to the ignored paths

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 
 var paths = {
   root: __dirname,
-  soften: ['**/*.js', '**/*.json', '!./node_modules/**'],
-  src: ['**/*.js', '!./node_modules/**'],
+  soften: ['**/*.js', '**/*.json', '!./node_modules/**', '!./docs/**'],
+  src: ['**/*.js', '!./node_modules/**', '!./docs/**'],
   specs: ['specs/**/*.js'],
   statements_threshold: 90,
   functions_threshold: 100,
